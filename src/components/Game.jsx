b93339e55b69f21e2066b4e0e85e1a2c15e1f3d2
mostafa <mostafameerzad@gmail.com>
2022-11-16 20:46:02 +0430

    change animation props to make a better result and remove redundant classNames from option-components

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -7,9 +7,11 @@ import { BgImg } from "../assets/images.jsx";
 import { motion, AnimatePresence } from "framer-motion";
 import { ChoiceContext } from "../App.jsx";
 
+
 // change the animation object based on mediaQuery if it is matching 
 // the desktop-view 
-const optionSelected = window.matchMedia("(min-width:1420px)").matches ? { top: "31.5%", left: "17%" } : { top: "31.5%", left: "33%" };
+const changeAnimation = window.matchMedia("(min-width:1024px)").matches
+const optionSelected ={ top: changeAnimation ? "26%" : "30%", left: "0%" }
 const animationDelay = 1;
 
 function Game({ setOption }) {
@@ -21,66 +23,79 @@ function Game({ setOption }) {
     machineChoice,
     isReset
   } = useContext(ChoiceContext);
+
+  console.log(loseOrWin, " lose or win from game component")
   return (
     <>
       <motion.section
         initial={{ rotate: 0, scale:0 }}
-        transition={{  ease: "circOut", duration: animationDelay }}
+        transition={{  ease: "circOut", duration: animationDelay, direction: "reverse"}}
         animate={{ rotate: 360, scale:1 }}
-        className="game-options "
+        className="game__options "
       >
         <motion.div
           animate={
             yourChoice
-              ? { rotate: 360, left: "-33%", top: "-.35rem" }
+              ? { rotate: 360, left:changeAnimation&&"-30%" }
               : isReset && {}
           }
           transition={{ duration: 0.5 }}
-          className="game-options__your-choice"
+          className="game__your-choice"
         >
-          <Option
+          <Option 
+          showHalo={loseOrWin === "you win" && yourChoice=== "paper" && true}
             img={paper}
             name={"paper"}
             setOption={setOption}
-            className="paper"
             callBack={setYourChoice}
             animate={yourChoice ? optionSelected : isReset && {}}
+           
           />
-          <Option
+          <Option 
+          showHalo={loseOrWin === "you win"  && yourChoice=== "scissors" && true}
             img={scissors}
             name={"scissors"}
             setOption={setOption}
-            className="scissors"
+            // className="scissors"
             callBack={setYourChoice}
             animate={yourChoice ? optionSelected : isReset && {}}
           />
-          <Option
+          <Option 
+          showHalo={loseOrWin === "you win"  && yourChoice=== "rock" && true}
             img={rock}
             name={"rock"}
             setOption={setOption}
-            className="rock"
+           
             callBack={setYourChoice}
             animate={yourChoice ? optionSelected : isReset && {}}
           />
 
           <motion.div
             animate={yourChoice ? { scale: 0 } : isReset && {}}
-            className="bg-img"
+            className="game__bg-img"
           >
             <BgImg />
           </motion.div>
+
+
         </motion.div>
+
 
         {machineChoice && (
           <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ delay: animationDelay }}
-            className="game-options__machine-choice"
+            
+            className="game__machine-choice"
           >
+          <motion.div initial={{opacity:0}}
+          animate={{opacity:.4}} className="game__machine-choice-placeholder"/>
+         
             <Option
-              name={`${machineChoice}`}
-              className={`${machineChoice}`}
+            showHalo={loseOrWin === "you lose" && true}
+              name={machineChoice}
+              initial={{opacity:0, }}
+              animate={{opacity:1}}
+              transition={{delay:1}}
+             
               img={
                 machineChoice === "paper"
                   ? paper
@@ -91,138 +106,23 @@ function Game({ setOption }) {
                   : ""
               }
             />
+   
           </motion.div>
-        )}
+        ) 
+        }
 
-        <div className="game-options__placeholder">
-          <div className="win">
 
-            {loseOrWin === "you win" && (
-              <svg
-                className="halo win__halo"
-                xmlns="http://www.w3.org/2000/svg"
-                width="200"
-                height="200"
-                viewBox="0 0 270 270"
-              >
-                <g id="halo" transform="translate(-768 -143)">
-                  <motion.circle
-                    initial={{ r: 0 }}
-                    animate={loseOrWin === "you win" && { r: 130 }}
-                    transition={{ delay: 1.1 }}
-                    className="halo__circle halo__circle--out"
-                    id="out"
-                    cx="135"
-                    cy="135"
-                    r="135"
-                    transform="translate(768 143)"
-                    fill="#fcfcfc99"
-                  />
+  
+      {loseOrWin && <motion.div initial={{opacity:0, scale:0}}
+      animate={{opacity:1, scale:1}} className="game__choice-type">
+        <p >you picked</p>
+        <p >the house picked</p>
+      </motion.div>}
 
-                  <motion.circle
-                    initial={{ r: 0 }}
-                    animate={loseOrWin === "you win" && { r: 95 }}
-                    transition={{ delay: 1.2 }}
-                    className="halo__circle halo__circle--mid"
-                    id="mid"
-                    cx="100.5"
-                    cy="100.5"
-                    r="90"
-                    transform="translate(803 178)"
-                    fill="#f3f3f399"
-                  />
 
-                  <motion.circle
-                    initial={{ r: 0 }}
-                    animate={loseOrWin === "you win" && { r: 65 }}
-                    transition={{ delay: 1.3 }}
-                    className="halo__circle halo__circle--in"
-                    id="in"
-                    cx="71"
-                    cy="71"
-                    r="65"
-                    transform="translate(833 207)"
-                    fill="#ebebeb99"
-                  />
-                </g>
-              </svg>
-            )}
-            <motion.div
-              initial={{ scale: 0 }}
-              animate={loseOrWin ? { scale: 1 } : {}}
-              transition={loseOrWin ? { delay: animationDelay } : {}}
-              className="message"
-            >
-              you picked
-            </motion.div>
-          </div>
-
-          <div className="lose">
-            {loseOrWin === "you lose" && (
-              <svg
-                className="halo lose__halo"
-                xmlns="http://www.w3.org/2000/svg"
-                width="200"
-                height="200"
-                viewBox="0 0 270 270"
-              >
-                <g id="halo" transform="translate(-768 -143)">
-                  <motion.circle
-                    initial={{ r: 0 }}
-                    animate={loseOrWin === "you lose" && { r: 130 }}
-                    transition={{ delay: 1.1 }}
-                    className="halo__circle halo__circle--out"
-                    id="out"
-                    cx="135"
-                    cy="135"
-                    r="135"
-                    transform="translate(768 143)"
-                    fill="#fcfcfc99"
-                  />
-
-                  <motion.circle
-                    initial={{ r: 0 }}
-                    animate={loseOrWin === "you lose" && { r: 95 }}
-                    transition={{ delay: 1.2 }}
-                    className="halo__circle halo__circle--mid"
-                    id="mid"
-                    cx="100.5"
-                    cy="100.5"
-                    r="90"
-                    transform="translate(803 178)"
-                    fill="#f3f3f399"
-                  />
-
-                  <motion.circle
-                    initial={{ r: 0 }}
-                    animate={loseOrWin === "you lose" && { r: 65 }}
-                    transition={{ delay: 1.3 }}
-                    className="halo__circle halo__circle--in"
-                    id="in"
-                    cx="71"
-                    cy="71"
-                    r="65"
-                    transform="translate(833 207)"
-                    fill="#ebebeb99"
-                  />
-                </g>
-              </svg>
-            )}
-            <motion.div
-              initial={{ scale: 0 }}
-              animate={loseOrWin ? { scale: 1 } : {}}
-              transition={loseOrWin ? { delay: animationDelay } : {}}
-              className="message"
-            >
-              the house picked
-            </motion.div>
-
-            {machineChoice && <div className="machineChoicePlaceHolder"></div>}
-          </div>
-        </div>
       </motion.section>
 
-      <section className="game-result-container">
+      <section className="result">
         <AnimatePresence>
           {loseOrWin && (
             <motion.div
@@ -234,12 +134,12 @@ function Game({ setOption }) {
                   : isReset && { scale: 0.5 }
               }
               exit={{ scale: 0 }}
-              className="game-result"
+              className="result__result"
             >
-              <div className="game-result__message">{loseOrWin}</div>
+              <div className="result__message">{loseOrWin}</div>
               <motion.button
                 whileTap={{ scale: 0.8 }}
-                className="game-result__btn btn"
+                className="result__play-again btn"
                 onClick={() => {
                   reset();
                 }}
