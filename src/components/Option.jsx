fe21a7df2337d7337e1840d01f8123d5361b70e1
mostafa <mostafameerzad@gmail.com>
2022-11-16 20:43:05 +0430

    include option halo into option-component itself

--- src/components/Option.jsx
+++ src/components/Option.jsx
@@ -1,33 +1,78 @@
 import { motion } from "framer-motion";
 
-
-
-function Option({ name, img,callBack, animate}) {
-
+function Option({ name, img, callBack, animate,initial, showHalo,transition  }) {
   return (
-
     <motion.div
       onClick={() => {
-        if(callBack){
-          callBack(name)
+        if (callBack) {
+          callBack(name);
         }
       }}
+      initial={initial}
       animate={animate}
-      whileTap={{scale:.8}}
-      className={
-           `game-options__option game-options__option--${name}`
-      }
+      transition={transition}
+      whileTap={{ scale: 0.9 }}
+      className={`game__option game__option--${name}`}
     >
       <motion.img
-      whileTap={{rotate:45}}
-        className="game-options__option-img"
+        whileTap={{ rotate: 45 }}
+        className="game__option-img"
         src={img}
         alt={name}
-      />      
+      />
+ {
+  showHalo && <svg
+     className="game__option-halo"
+     xmlns="http://www.w3.org/2000/svg"
+     width="200"
+     height="200"
+     viewBox="0 0 270 270"
+   >
+     <g id="halo" transform="translate(-768 -143)">
+       <motion.circle
+         initial={{ r: 0 }}
+         animate={ { r: 110 }}
+         transition={{ delay: 1.1 }}
+         className="game__option-halo--out"
+         id="out"
+         cx="135"
+         cy="135"
+         transform="translate(768 143)"
+         fill="#fcfcfc99"
+         opacity={.04}
+
+       />
+
+       <motion.circle
+         initial={{ r: 0 }}
+         animate={ { r:84 }}
+         transition={{ delay: 1.2 }}
+         className="game__option-halo--mid"
+         id="mid"
+         cx="100.5"
+         cy="100.5"
+         transform="translate(803 178)"
+         fill="#f3f3f399"
+         opacity={.05}
+       />
+
+       <motion.circle
+         initial={{ r: 0 }}
+         animate={ { r: 63 }}
+         transition={{ delay: 1.3 }}
+         className="game__option-halo--in"
+         id="in"
+         cx="71"
+         cy="71"
+         transform="translate(833 207)"
+         fill="#ebebeb99"
+         opacity={.04}
+
+       />
+     </g>
+    </svg> }
     </motion.div>
   );
 }
 
 export default Option;
-
-
