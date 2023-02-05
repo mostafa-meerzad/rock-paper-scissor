811b1a9c78fc1b1de362fd8ea0dcc3c750ffd3b1
mostafa <mostafameerzad@gmail.com>
2023-02-05 02:35:25 -0800

    make score-value animate when it changes

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,7 +1,8 @@
 import { Logo } from "../assets/images";
+import { motion } from 'framer-motion';
 
 
-function Score({ scoreValue, initialScore }) {
+function Score({ scoreValue }) {
 
   return (
     <section className={"score"}>
@@ -10,9 +11,10 @@ function Score({ scoreValue, initialScore }) {
         className="score__container"
       >
         <p className={"score__container-title"}>score</p>
-        <p className="score__container-value">
-          {" "}{scoreValue}
-        </p>
+        {/* change key prop of the element showing score-value to make react treat it as a new element in order to play it's animation every time score changes */}
+        <motion.p key={scoreValue} initial={{scale:.5}} animate={{scale:1}} className="score__container-value">
+          {scoreValue}
+        </motion.p>
       </div>
     </section>
   );
