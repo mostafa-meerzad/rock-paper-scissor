3035f21793695eba026ad57a9cbed456f958e339
mostafa <mostafameerzad@gmail.com>
2023-02-05 02:34:29 -0800

    Remove unnecessary props

--- src/components/Option.jsx
+++ src/components/Option.jsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 
-function Option({ name, img, callBack, animate,initial, showHalo,transition  }) {
+function Option({ name, img, callBack, animate, showHalo }) {
   return (
     <motion.div
       onClick={() => {
@@ -8,10 +8,8 @@ function Option({ name, img, callBack, animate,initial, showHalo,transition  }) 
           callBack(name);
         }
       }}
-      initial={initial}
       animate={animate}
-      transition={transition}
-      whileTap={{ scale: 0.9 }}
+      whileTap={!showHalo && { scale: 0.9 }}
       className={`game__option game__option--${name}`}
     >
       <motion.img
@@ -20,57 +18,56 @@ function Option({ name, img, callBack, animate,initial, showHalo,transition  }) 
         src={img}
         alt={name}
       />
- {
-  showHalo && <svg
-     className="game__option-halo"
-     xmlns="http://www.w3.org/2000/svg"
-     width="200"
-     height="200"
-     viewBox="0 0 270 270"
-   >
-     <g id="halo" transform="translate(-768 -143)">
-       <motion.circle
-         initial={{ r: 0 }}
-         animate={ { r: 110 }}
-         transition={{ delay: 1.1 }}
-         className="game__option-halo--out"
-         id="out"
-         cx="135"
-         cy="135"
-         transform="translate(768 143)"
-         fill="#fcfcfc99"
-         opacity={.04}
+      {showHalo && (
+        <svg
+          className="game__option-halo"
+          xmlns="http://www.w3.org/2000/svg"
+          width="200"
+          height="200"
+          viewBox="0 0 270 270"
+        >
+          <g id="halo" transform="translate(-768 -143)">
+            <motion.circle
+              initial={{ r: 0 }}
+              animate={{ r: 110 }}
+              transition={{ delay: 1.1 }}
+              className="game__option-halo--out"
+              id="out"
+              cx="135"
+              cy="135"
+              transform="translate(768 143)"
+              fill="#fcfcfc99"
+              opacity={0.04}
+            />
 
-       />
+            <motion.circle
+              initial={{ r: 0 }}
+              animate={{ r: 84 }}
+              transition={{ delay: 1.2 }}
+              className="game__option-halo--mid"
+              id="mid"
+              cx="100.5"
+              cy="100.5"
+              transform="translate(803 178)"
+              fill="#f3f3f399"
+              opacity={0.05}
+            />
 
-       <motion.circle
-         initial={{ r: 0 }}
-         animate={ { r:84 }}
-         transition={{ delay: 1.2 }}
-         className="game__option-halo--mid"
-         id="mid"
-         cx="100.5"
-         cy="100.5"
-         transform="translate(803 178)"
-         fill="#f3f3f399"
-         opacity={.05}
-       />
-
-       <motion.circle
-         initial={{ r: 0 }}
-         animate={ { r: 63 }}
-         transition={{ delay: 1.3 }}
-         className="game__option-halo--in"
-         id="in"
-         cx="71"
-         cy="71"
-         transform="translate(833 207)"
-         fill="#ebebeb99"
-         opacity={.04}
-
-       />
-     </g>
-    </svg> }
+            <motion.circle
+              initial={{ r: 0 }}
+              animate={{ r: 63 }}
+              transition={{ delay: 1.3 }}
+              className="game__option-halo--in"
+              id="in"
+              cx="71"
+              cy="71"
+              transform="translate(833 207)"
+              fill="#ebebeb99"
+              opacity={0.04}
+            />
+          </g>
+        </svg>
+      )}
     </motion.div>
   );
 }
